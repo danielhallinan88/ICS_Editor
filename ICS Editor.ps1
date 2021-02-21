0bbe75bbf4ea3a4cb4f8da634c28071abaa8c367
unknown <danielhallinan88@gmail.com>
2021-02-21 12:24:11 -0600

    sorted

--- ICS Editor.ps1
+++ ICS Editor.ps1
@@ -12,7 +12,7 @@ $output_path = $desktop_path + "\" + $new_filename
 
 $form = New-Object System.Windows.Forms.Form;
  $form.Width = 500;
- $form.Height = 250;
+ $form.Height = 700;
  $form.Text = $title;
  $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
 
@@ -49,24 +49,37 @@ $form = New-Object System.Windows.Forms.Form;
  $event_list = New-Object System.Windows.Forms.ListBox
  $event_list.Left = 25
  $event_list.Top = 40
- $event_list.Width = 405
+ $event_list.Width = 435
+ $event_list.Height = 500
  $event_list.SelectionMode = 'MultiExtended'
+
+ $search_button = New-Object System.Windows.Forms.Button
+ $search_button.Left = 235
+ $search_button.Top = 563
+ $search_button.Width = 60
+ $search_button.Text = 'Search'
+
+ $search_textbox = New-Object System.Windows.Forms.TextBox;
+ $search_textbox.Left = 25;
+ $search_textbox.Top = 565;
+ $search_textbox.width = 200;
+ $search_textbox.Text = "";
 
  $remove_button = New-Object System.Windows.Forms.Button
  $remove_button.Left = 25
- $remove_button.Top = 145
+ $remove_button.Top = 595
  $remove_button.Width = 125
  $remove_button.Text = 'Remove Selected'
 
  $save_button = New-Object System.Windows.Forms.Button
  $save_button.Left = 25
- $save_button.Top = 175
+ $save_button.Top = 625
  $save_button.Width = 60
  $save_button.Text = 'Save As'
 
  $text_label2 = New-Object System.Windows.Forms.Label;
  $text_label2.Left = 90
- $text_label2.Top = 180
+ $text_label2.Top = 630
  $text_label2.Width = 225
  $text_label2.Text = $output_path;
 
@@ -107,10 +120,25 @@ function Parse-ICS {
         [string]$ics_content
     )
 
-    $header_regex = "BEGIN[\s\S]*END:VTIMEZONE"
-    $header = Select-String -InputObject $ics_contents -AllMatches $header_regex
-    New-Variable -Name header_text -Value $($header.Matches.Groups[0].ToString()) -Scope Script -Force
+    $lines = $ics_content -split "`r`n"
 
+    $header_output = ""
+    foreach($line in $lines){
+
+        if ($line.StartsWith("BEGIN:VEVENT")){
+            break
+        }
+        else {
+            $header_output += $line + "`r`n"
+        }
+    }
+    #$header_output.Trim()
+
+    #$header_regex = "BEGIN[\s\S]*END:VTIMEZONE"
+    #$header = Select-String -InputObject $ics_contents -AllMatches $header_regex
+
+    #New-Variable -Name header_text -Value $($header.Matches.Groups[0].ToString()) -Scope Script -Force
+    New-Variable -Name header_text -Value $($header_output) -Scope Script -Force
     $event_regex = "BEGIN:VEVENT[\s|\S]*?END:VEVENT"
     $events = Select-String -InputObject $ics_contents -AllMatches $event_regex
 
@@ -138,11 +166,22 @@ $open_button.Add_Click(
 
         [string]$ics_contents = Get-Content -Path $ics_path -Raw
 
-        $header, $events, $footer = Parse-ICS -ics_content $ics_content
+        $header, $events, $footer = Parse-ICS -ics_content $ics_contents
 
         $titles = $events | foreach { $_.title }
-        $event_list.Items.AddRange($titles)
+        $titles_sorted = $titles | Sort-Object
+        $event_list.Items.AddRange($titles_sorted)
+
+        #$event_list.Sorted = true
         
+    }
+)
+
+$search_button.Add_Click(
+    {
+        $search_string = $search_textbox.Text
+        write-host $search_string
+        $event_list.Items.Contains($search_string)
     }
 )
 
@@ -160,7 +199,7 @@ $remove_button.Add_Click(
                 $new_items += $item
              }
         }
-
+        
         $event_list.Items.Clear()
         $event_list.Items.AddRange($new_items)
 
@@ -169,6 +208,10 @@ $remove_button.Add_Click(
 
 $save_button.Add_Click(
     {
+        write-host $header_text
+        Write-Host $events_hashes[0].raw_text
+        write-host $footer
+
         $items_to_write = $event_list.Items
         $items_to_write_hashes = @()
 
@@ -188,6 +231,7 @@ $save_button.Add_Click(
         $final_ics += $footer
 
         Out-File -FilePath $output_path -InputObject $final_ics -Encoding ASCII
+        #(gc $output_path) | ? {$_.trim() -ne "" } | set-content $output_path
 
     }
 )
@@ -202,6 +246,8 @@ $save_button.Add_Click(
  $form.Controls.Add($ics_file);
  $form.Controls.Add($event_list);
 
+ #$form.Controls.Add($search_button)
+ #$form.Controls.Add($search_textbox)
  $form.Controls.Add($save_button)
  $form.Controls.Add($remove_button)
  $form.Controls.Add($text_label2)
